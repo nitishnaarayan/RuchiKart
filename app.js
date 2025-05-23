1c579c7de0e14759655adbe950dd622317727682
unknown <you@example.com>
2025-05-23 12:10:42 +0530

    Eposide 1

---
+++ app.js
@@ -0,0 +1,13 @@
+const parent = React.createElement("div", { id: "parent" }, [
+	React.createElement("div", { id: "child" }, [
+		React.createElement("h1", {}, "This is H1"),
+		React.createElement("h2", {}, "This is H2"),
+	]),
+	React.createElement("div", { id: "parent" }, [
+		React.createElement("h2", {}, "this is h1 for p for div 2"),
+		React.createElement("h3", {}, "This is h3"),
+	]),
+]);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
